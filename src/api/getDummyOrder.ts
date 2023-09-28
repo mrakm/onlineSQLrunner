2f02ce15cdef0324001d976805bc0292602162c6
mrakm <theawesomeashutosh@gmail.com>
2023-09-28 05:58:10 +0530

    change file name

--- src/api/getDummyOrder.ts
+++ src/api/getDummyOrder.ts
@@ -1,5 +1,5 @@
 
-const OUTPUT_FILE_PATH='src/data/orders.csv'
+const OUTPUT_FILE_PATH='./orders.csv'
 export const getDummyOrder = async () => {
     const response = await fetch(`/api/readCSV?filename=${OUTPUT_FILE_PATH}`);
     const data = await response.json() as Array<{[key: string]: string}>;
